dd755a7c4b5f511d73d4946fb3f2a75262cae5d6
Yang Jun <ouyang.qianba@gmail.com>
2018-11-17 23:13:09 +0800

    :bug: [fix] package ignore

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -1,15 +1,21 @@
 const os = require('os')
 const {resolve} = require('path')
 const packager = require('electron-packager')
+const package = require('../../package.json')
+const devDependencies = Object.entries(package.devDependencies || {})
+  .map(([d])=>`/node_modules/${d}`)
 packager({
   dir: resolve(`${__dirname}/../../`),
   platform: os.platform(),
+  out:"pkg",
   ignore(path) {
     return !(
+      path === "/node_modules" ||
       path === "" ||
       path.startsWith('/dist') ||
       path === "/package.json"
-    )
+    ) ||
+    (devDependencies.some(d=>path.startsWith(d)))
   },
   overwrite: true
 })

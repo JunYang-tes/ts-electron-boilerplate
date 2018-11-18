05aee6a493b99f176c8d341e31141529ccb85561
Yang Jun <ouyang.qianba@gmail.com>
2018-11-18 15:33:06 +0800

    :bug: [fix] disable hot in production mode

--- src/render/index.tsx
+++ src/render/index.tsx
@@ -1,10 +1,18 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import {hot} from 'react-hot-loader'
 import App from './app'
 
-const ReloadableApp = hot(module)(App)
-ReactDOM.render(
-  <ReloadableApp />,
-  document.getElementById('root'),
-)
+if (process.env.NODE_ENV === 'development') {
+  // tslint:disable-next-line
+  const hot = require('react-hot-loader')
+  const ReloadableApp = hot(module)(App)
+  ReactDOM.render(
+    <ReloadableApp />,
+    document.getElementById('root'),
+  )
+} else {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+  )
+}
